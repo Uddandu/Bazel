9b89dbbf161d9d9ed5cc4033b149b9c0fff113df
uddandu Shaik <uddandu669@gmail.com>
2026-08-15 23:06:13 +0530

    Initial Bazel project

---
+++ bazel-bazel_p2/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "calculator.h"
+
+int main()
+{
+    std::cout << "Add      : " << add(10, 20) << '\n';
+    std::cout << "Multiply : " << multiply(10, 20) << '\n';
+
+    return 0;
+}
